d4d73c5b3e245167f08d739bb13e5cd1368aeb4e
ionchi <ion.chiriac@live.com>
2021-09-10 17:24:49 +0200

    new readme; compress images

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ export const Navbar = () => {
 			sx={ { borderBottom: (theme) => `1px solid ${theme.palette.divider}` } }
 		>
 			<Toolbar sx={ { flexWrap: 'wrap' } }>
-				<Typography variant="h6" color="inherit" noWrap sx={ { flexGrow: 1 } }>
+				<Typography variant="h6" color="inherit" noWrap sx={ { flexGrow: 1 } } fontWeight={ 600 }>
 					BotBuilder
 				</Typography>
 				<nav>
